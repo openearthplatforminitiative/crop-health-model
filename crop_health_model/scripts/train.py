52d5560790741955b6f9194f4ed4177b1c4ecfda
Gil Tinde <gil.tinde@knowit.no>
2024-03-27 14:57:55 +0100

    remove unused imports

--- crop_health_model/scripts/train.py
+++ crop_health_model/scripts/train.py
@@ -1,18 +1,14 @@
-import lightning.pytorch as pl
-from lightning.pytorch import seed_everything
+import torch
 from lightning.pytorch.cli import LightningCLI
 
 from crop_health_model.datasets.datamodule import CropHealthDataModule
 from crop_health_model.engines.system import LitModel
-from crop_health_model.models.model import ResNet
 
-crop_health_data_module = CropHealthDataModule
+torch.set_float32_matmul_precision("medium")
 
 
 def main():
-    cli = LightningCLI(
-        model_class=LitModel, datamodule_class=CropHealthDataModule
-    )  # , subclass_mode_model=True)
+    cli = LightningCLI(model_class=LitModel, datamodule_class=CropHealthDataModule)
 
 
 if __name__ == "__main__":
