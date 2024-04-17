01edef9970aacb887af66c6d2bea6bda97836dd7
Gil Tinde <31830765+giltinde@users.noreply.github.com>
2024-04-17 17:12:27 +0200

    no saving hyperparameters

--- crop_health_model/engines/system.py
+++ crop_health_model/engines/system.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class LitModel(pl.LightningModule):
     """PyTorch Lightning module for training the Crop Health model."""
 
@@ -12,19 +13,15 @@ class LitModel(pl.LightningModule):
     ) -> None:
         super(LitModel, self).__init__()
         self.model = model
-        hyperparameters = self.model.get_hyperparameters()
-        self.save_hyperparameters(hyperparameters)
 
     def forward(self, x) -> torch.Tensor:
         return self.model(x)
 
-    def _compute_loss(
-        self, logits: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    def _compute_loss(self, logits: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         """Compute the loss function."""
         return F.cross_entropy(logits, y)
 
-    def step_wrapper(
-        self, batch: tuple, batch_idx: int) -> torch.Tensor:
+    def step_wrapper(self, batch: tuple, batch_idx: int) -> torch.Tensor:
         """Wrapper for the training/validation/test step."""
         x, y = batch
         logits = self.model(x)
