1e3143c564555352fe457854ba1e75041ef46d73
Gil Tinde <31830765+giltinde@users.noreply.github.com>
2024-04-18 12:11:01 +0200

    compute class weights

--- crop_health_model/engines/system.py
+++ crop_health_model/engines/system.py
@@ -13,22 +13,35 @@ class LitModel(pl.LightningModule):
     ) -> None:
         super(LitModel, self).__init__()
         self.model = model
+        self.class_weights = None
 
     def forward(self, x) -> torch.Tensor:
         return self.model(x)
 
-    def _compute_loss(self, logits: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-        """Compute the loss function."""
-        return F.cross_entropy(logits, y)
+    def set_class_weights(self, class_weights: torch.Tensor) -> None:
+        """Set the class weights for the loss function."""
+        self.class_weights = class_weights
 
-    def step_wrapper(self, batch: tuple, batch_idx: int) -> torch.Tensor:
+    def _compute_loss(
+        self, logits: torch.Tensor, y: torch.Tensor, weights=None
+    ) -> torch.Tensor:
+        """Compute the loss function."""
+        return F.cross_entropy(logits, y, weight=weights)
+
+    def step_wrapper(
+        self, batch: tuple, batch_idx: int, use_weights: bool = False
+    ) -> torch.Tensor:
         """Wrapper for the training/validation/test step."""
         x, y = batch
         logits = self.model(x)
-        return self._compute_loss(logits, y)
+        if use_weights:
+            loss = self._compute_loss(logits, y, self.class_weights)
+        else:
+            loss = self._compute_loss(logits, y)
+        return loss
 
     def training_step(self, batch: tuple, batch_idx: int) -> torch.Tensor:
-        return self.step_wrapper(batch, batch_idx)
+        return self.step_wrapper(batch, batch_idx, use_weights=True)
 
     def validation_step(self, batch: tuple, batch_idx: int) -> torch.Tensor:
         return self.step_wrapper(batch, batch_idx)
