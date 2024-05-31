d6bac8e85aec09be4079b54ab16d868882f97f5d
Gil Tinde <31830765+giltinde@users.noreply.github.com>
2024-05-31 17:19:50 +0200

    fix: update dockerfile and script to run

--- build_image/docker/dockerd-entrypoint.sh
+++ build_image/docker/dockerd-entrypoint.sh
@@ -4,9 +4,20 @@ set -e
 if [[ "$1" = "serve" ]]; then
     shift 1
     torchserve --start --ts-config /home/model-server/config.properties
+    echo "TorchServe started, waiting for it to stabilize..."
+    sleep 10  # Waits for 10 seconds before proceeding
 else
     eval "$@"
 fi
 
 # prevent docker exit
-tail -f /dev/null
+# tail -f /dev/null
+
+# Start FastAPI app
+uvicorn api.redoc:app --host 0.0.0.0 --port 5000 &
+
+# Prevent Docker container from exiting
+wait -n
+
+# Exit with status of process that exited first
+exit $?
