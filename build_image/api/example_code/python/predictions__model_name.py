bd3d29b12550b2c78c94b979dc361a1aa83c0b02
Gil Tinde <31830765+giltinde@users.noreply.github.com>
2024-06-06 11:45:46 +0200

    formatting

--- build_image/api/example_code/python/predictions__model_name.py
+++ build_image/api/example_code/python/predictions__model_name.py
@@ -4,7 +4,7 @@ with Client() as client:
     # Get the prediction for image cocoa.jpg passed as a binary file in the request body
     response_binary = client.post(
         url="$api_url" + "/predictions/binary",
-        body=open("cocoa.jpg", "rb").read(), 
+        body=open("cocoa.jpg", "rb").read(),
     )
 
     data_binary = response_binary.json()
@@ -13,7 +13,7 @@ with Client() as client:
     # Get the prediction for image cocoa.jpg passed as a binary file in the request body
     response_single_HLT = client.post(
         url="$api_url" + "/predictions/single-HLT",
-        body=open("cocoa.jpg", "rb").read(), 
+        body=open("cocoa.jpg", "rb").read(),
     )
 
     data_single_HLT = response_single_HLT.json()
@@ -22,7 +22,7 @@ with Client() as client:
     # Get the prediction for image cocoa.jpg passed as a binary file in the request body
     response_multi_HLT = client.post(
         url="$api_url" + "/predictions/multi-HLT",
-        body=open("cocoa.jpg", "rb").read(), 
+        body=open("cocoa.jpg", "rb").read(),
     )
 
     data_multi_HLT = response_multi_HLT.json()
