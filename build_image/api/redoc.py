846a74e493e2fc4680214138d4ab029d4bbaf49f
Gil Tinde <31830765+giltinde@users.noreply.github.com>
2024-06-06 11:44:26 +0200

    fix: don't use deprecated on_event("startup")

--- build_image/api/redoc.py
+++ build_image/api/redoc.py
@@ -1,49 +1,39 @@
 import pathlib
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import HTMLResponse 
+from contextlib import asynccontextmanager
+
 import requests
 from api.custom_openapi import custom_openapi_gen
-
-app = FastAPI(openapi_url=None)
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
 
 example_code_dir = pathlib.Path(__file__).parent / "example_code"
-
-# Cache the OpenAPI JSON
 openapi_json_cache = None
 
-@app.on_event("startup")
-async def load_openapi_json():
+
+@asynccontextmanager
+async def app_lifespan(app):
     global openapi_json_cache
-    response = requests.options("http://localhost:8080/", timeout=10)
-    # log the response
-    if response.status_code == 200:
-        openapi_json_cache = response.json()
-        print("Response from TorchServe")
-        print(openapi_json_cache)
-    else:
-        raise Exception("Failed to load OpenAPI JSON from TorchServe")
-    
+    try:
+        response = requests.options("http://localhost:8080/", timeout=10)
+        if response.status_code == 200:
+            openapi_json = response.json()
+            # Remove specific endpoints if needed
+            endpoints_to_keep = ["/predictions/{model_name}", "/ping"]
+            all_endpoints = list(openapi_json["paths"].keys())
+            for endpoint in all_endpoints:
+                if endpoint not in endpoints_to_keep:
+                    del openapi_json["paths"][endpoint]
+            openapi_json = custom_openapi_gen(openapi_json, example_code_dir)
+            openapi_json_cache = openapi_json
+        else:
+            raise Exception("Failed to load OpenAPI JSON from TorchServe")
+        yield
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-@app.on_event("startup")
-async def load_openapi_json():
-    global openapi_json_cache
-    response = requests.options("http://localhost:8080/", timeout=10)
-    if response.status_code == 200:
-        openapi_json = response.json()
-        
-        # Remove specific endpoints if needed
-        endpoints_to_keep = ["/predictions/{model_name}", "/ping"]
-        all_endpoints = list(openapi_json["paths"].keys())
-        for endpoint in all_endpoints:
-            if endpoint not in endpoints_to_keep:
-                del openapi_json["paths"][endpoint]
 
-        openapi_json = custom_openapi_gen(openapi_json, example_code_dir)
+app = FastAPI(openapi_url=None, lifespan=app_lifespan)
 
-        openapi_json_cache = openapi_json
-        print("Modified OpenAPI JSON loaded")
-    else:
-        raise Exception("Failed to load OpenAPI JSON from TorchServe")
 
 @app.get("/torchserve-openapi.json")
 async def get_openapi_json():
@@ -51,6 +41,7 @@ async def get_openapi_json():
         return openapi_json_cache
     else:
         raise HTTPException(status_code=500, detail="OpenAPI JSON is not loaded")
+
 
 @app.get("/redoc", response_class=HTMLResponse)
 async def redoc():
@@ -71,6 +62,8 @@ async def redoc():
     """
     return redoc_html
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=5000)
