b27cbd07a831ced7870cd7c26ca57fb748ce5e10
Gil Tinde <31830765+giltinde@users.noreply.github.com>
2024-06-14 15:45:47 +0200

    fix: remove all docker/deployment logic

--- build_image/api/example_code/javascript/ping.js
+++
@@ -1,2 +0,0 @@
-const response = await fetch("$api_url/ping");
-const data = await response.json();
--- build_image/api/example_code/python/ping.py
+++
@@ -1,5 +0,0 @@
-from httpx import Client
-
-with Client() as client:
-    response = client.get(url="$api_url" + "/ping")
-    data = response.json()
