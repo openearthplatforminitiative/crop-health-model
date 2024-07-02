2702dc1e5de3dc552547bc290050984f627f093d
Gil Tinde <31830765+giltinde@users.noreply.github.com>
2024-07-02 14:19:28 +0200

    fix: adjust topk values in saved handler scripts

--- tb_logs/multi_HLT/version_0/model_handler.py
+++ tb_logs/multi_HLT/version_0/model_handler.py
@@ -3,6 +3,7 @@ from ts.torch_handler.image_classifier import ImageClassifier
 
 
 class CustomHandler(ImageClassifier):
+    topk = 17
     image_processing = transforms.Compose(
         [
             transforms.Resize(interpolation=2, size=256),
